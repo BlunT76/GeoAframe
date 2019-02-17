fa71c7d9e59d668e2abab0d494822eec5344dea5
philippe <huhmiel@free.fr>
2019-02-17 19:28:20 +0100

    svg image as file

--- src/components/BtnMenu.js
+++ src/components/BtnMenu.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { ReactComponent as MenuSvgIcon } from '../img/bars-solid.svg';
 
 const BtnMenu = (props) => {
   const { handleSubmit } = props;
   return (
     <button type="button" className="btn btn-light BtnMenu" onClick={handleSubmit}>
-      <svg size="5x" aria-hidden="true" data-prefix="fas" data-icon="bars" className="svg-inline--fa fa-bars fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-        <path fill="currentColor" d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z" />
-      </svg>
+      <MenuSvgIcon />
     </button>
   );
 };
